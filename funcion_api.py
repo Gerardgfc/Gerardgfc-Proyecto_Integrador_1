12128f31f7cdebf6e1c43928c5d5913124f18aa5
GerardGc <gerardo.carrizo123.gc[1;7rgmail.com>
2024-01-07 14:19:10 -0300

    17° Actualización datos

--- funcion_api.py
+++ funcion_api.py
@@ -19,82 +19,81 @@ def presentacion():
     str: Código HTML que muestra la página de presentación.
     '''
     return '''
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Explora la API de Steam</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-image: url('https://i.blogs.es/400138/steam-boicot-reddit/1366_2000.png');
-            background-attachment: fixed;
-            background-color: #f0f0f0;
-            color: #333;
-            margin: 0;
-            padding: 0;
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Explora la API de Steam</title>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                background-image: url('https://i.blogs.es/400138/steam-boicot-reddit/1366_2000.png');
+                background-attachment: fixed;
+                background-color: #f0f0f0;
+                color: #333;
+                margin: 0;
+                padding: 0;
+            }
 
-        header {
-            background-color: #333;
-            color: white;
-            padding: 18.5px;
-            text-align: center;
-        }
+            header {
+                background-color: #333;
+                color: white;
+                padding: 18.5px;
+                text-align: center;
+            }
 
-        main {
-            max-width: 800px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: rgba(255, 255, 255, 0.8);
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+            main {
+                max-width: 800px;
+                margin: 20px auto;
+                padding: 20px;
+                background-color: rgba(255, 255, 255, 0.8);
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
 
-        a {
-            color: #007bff;
-            text-decoration: none;
-        }
+            a {
+                color: #007bff;
+                text-decoration: none;
+            }
 
-        a:hover {
-            text-decoration: underline;
-        }
+            a:hover {
+                text-decoration: underline;
+            }
 
-        footer {
-            text-align: center;
-            margin-top: 20px;
-            padding: 10px;
-            background-color: #333;
-            color: white;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>Explora la API de Steam</h1>
-    </header>
-    
-    <main>
-        <h2>¡Explora la API de Steam para obtener información sobre videojuegos! Realiza diversas consultas directamente desde la plataforma.</h2>
+            footer {
+                text-align: center;
+                margin-top: 20px;
+                padding: 10px;
+                background-color: #333;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Explora la API de Steam</h1>
+        </header>
         
-        <h3><strong>Cómo empezar:</strong></h3>
-        <ul>
-            <li>Agrega <span style="background-color: lightgray;">/docs</span> al final de la URL actual para explorar e interactuar con la API.</li>
-            <li>O simplemente hacé <a href="https://proyecto-integrador-1.onrender.com/docs" target="_blank">click acá</a> para acceder a la documentación.</li>
-        </ul>
+        <main>
+            <h2>¡Explora la API de Steam para obtener información sobre videojuegos! Realiza diversas consultas directamente desde la plataforma.</h2>
+            
+            <h3><strong>Cómo empezar:</strong></h3>
+            <ul>
+                <li>Agrega <span style="background-color: lightgray;">/docs</span> al final de la URL actual para explorar e interactuar con la API.</li>
+                <li>O simplemente hacé <a href="https://proyecto-integrador-1.onrender.com/docs" target="_blank">click acá</a> para acceder a la documentación.</li>
+            </ul>
 
-        <p>Si deseas conocer más sobre el proyecto, puedes visitar mi perfil en <a href="https://www.linkedin.com/in/gerard-carrizo-508b16133/" target="_blank">LinkedIn</a>.</p>
+            <p>Si deseas conocer más sobre el proyecto, puedes visitar mi perfil en <a href="https://www.linkedin.com/in/gerard-carrizo-508b16133/" target="_blank">LinkedIn</a>.</p>
 
-        <p>El desarrollo completo está disponible en <a href="https://github.com/Gerardgfc/Gerardgfc-Proyecto_Integrador_1" target="_blank">GitHub</a>.</p>
-    </main>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <footer>
-        <p>&copy; 2023 Gerardo Carrizo</p>
-    </footer>
-</body>
-</html>
+            <p>El desarrollo completo está disponible en <a href="https://github.com/Gerardgfc/Gerardgfc-Proyecto_Integrador_1" target="_blank">GitHub</a>.</p>
+        </main>
+        <br>
+        <br>
+        <br>
+        <br>
+        <footer>
+            <p>&copy; 2023 Gerardo Carrizo</p>
+        </footer>
+    </body>
+    </html>
     '''
 
 
@@ -119,7 +118,7 @@ def PlayTimeGenre(genre: str):
 def UserForGenre(genre: str) -> dict:
     """Devuelve el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
     
-    Genre: Genero a obtener el número de usuarios que han jugado
+    Genre: Género para obtener el número de usuarios que han jugado.
 
     Returns:
         Usuario que ha jugado más horas para un género específico teniendo en cuenta los géneros disponibles.
@@ -129,10 +128,21 @@ def UserForGenre(genre: str) -> dict:
         return {"Error": f"Género {genre} no encontrado en el dataset."}
     else:
         genre_df = df_games_items[df_games_items[genre] == 1]
+
+        # Convertir playtime_forever de minutos a horas
+        genre_df['playtime_forever'] = (genre_df['playtime_forever'] / 60).round(2)
+
+        # Obtener usuario con más horas jugadas
         user_playtime_df = genre_df.groupby('user_id')['playtime_forever'].sum().reset_index()
         max_playtime_user = user_playtime_df.loc[user_playtime_df['playtime_forever'].idxmax(), 'user_id']
-        return {"Género": genre, f"Usuario con más horas jugadas para Género {genre} :": max_playtime_user}
 
+        # Obtener acumulación de horas jugadas por año
+        yearly_playtime = genre_df.groupby('year')['playtime_forever'].sum().reset_index()
+
+        return {
+            "Género": genre,
+            f"Usuario con más horas jugadas para Género {genre}": max_playtime_user,
+            "Acumulación de horas jugadas por año": yearly_playtime.to_dict(orient='records')}
 
 def UsersRecommend( year : int ):
     """Devuelve el top 3 de juegos más recomendados por usuarios para el año dado.
